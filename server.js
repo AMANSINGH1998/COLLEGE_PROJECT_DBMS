bff5b0c8d8f9541aaf5008dd9382b0b1715e3844
Aman Singh <amansingh19981998gmail.com>
2018-08-15 20:36:15 +0530

    first stage Database
    Signed-off-by: Aman Singh <amansingh19981998gmail.com>

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
+const express=require('express')
+const app=express()
 const Sequelize=require("sequelize")
 const Datatypes=Sequelize.DataTypes
 const db=new Sequelize(
     {
         dialect:'sqlite',
-        storage:__dirname + '/restaraunt.db'
+        storage:__dirname + '/restaraunts.db'
     })
-    const orders=db.define('ORDER',
+    const Order=db.define('ORDER',
     {
         Table_no:Datatypes.INTEGER,
         order:Datatypes.STRING,
@@ -18,3 +20,41 @@ const db=new Sequelize(
     }).catch((err)=>{
         console.error
     })
+
+// $(function()
+// {
+    
+//     let input=$("#placeorders")
+//     let MRP=$("#price")
+//     let button=$("button")
+//     button.click(function()
+//     {
+//         console.log("clicked")
+//     })
+app.use('/',express.static(__dirname+'/public'))
+app.get('/insert',(req,res)=>
+{
+    Order.create({
+        Table_no:req.query["table"],
+        order:req.query["placeorder"],
+        price:req.query["price"]
+     })
+console.log("insert")
+console.log(req.query['table'])
+})
+ 
+app.listen(5455,function()
+{
+    console.log(`connected
+    http://localhost:5455
+    `)
+})
+// })
+ 
+    // db.findALL(
+    // {
+    //     where:
+    //     {
+    //         Table_no:1
+    //     }
+    // })
