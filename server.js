7605d6ca1a348aa134648d10912010701a410518
Aman Singh <amansingh19981998gmail.com>
2018-09-29 21:15:23 +0530

    UPDATE:1 PROJECT

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const Datatypes=Sequelize.DataTypes
 const db=new Sequelize(
     {
         dialect:'sqlite',
-        storage:__dirname + '/restarauntstestFINAL.db'
+        storage:__dirname + '/restarauntstestsirtest4.db'
     })
     const Order=db.define('ORDER',
     {
@@ -59,22 +59,63 @@ console.log(req.query['table'])
    
 //         })
 
-app.get('/bill',(req,res)=>
-{
+// app.get('/bill',(req,res)=>
+// {
 
 
-    Order.sum('price' ,{where:{Table_no:req.query["fbill"] }}).then(Order1=>
-        {
-            // console.log("HEY I AM HERE");
+//     Order.sum('price' ,{where:{Table_no:req.query["fbill"] }}).then(Order1=>
+//         {
+//             // console.log("HEY I AM HERE");
             
-           res.send(`<h1 align="center" style="font-weight:100;margin:11%;letter_spacing:17">AMOUNT  :  ${Order1}</h1>`);
-           console.log(Order1.order);
+//            res.send(`<h1 align="center" style="font-weight:100;margin:11%;letter_spacing:17">AMOUNT  :  ${Order1}</h1>`);
+//            console.log(Order1.order);
     
-        })
+//         })
     
+
+// })
+
+
+app.get('/bill',(req, res)=>
+{
+    Order.findAll({where: {Table_no:req.query["fbill"]  }}).then(ans=>
+    {
+        console.log(ans)
+        
+        
+        
+        const str = ans.reduce((acc, val) => {
+            return acc + `<li> ${val.order} </li>`
+        }, '')
+        const ans1=ans.reduce((acc,val)=>
+        {
+            return acc+ +val.price
+
+
+
+        },0);
+
+        
+        res.send(`
+            <html>
+                <body>
+                <div align="center">
+                <h1 align="center" style="letter-spacing:17px;font-weight:100">YOUR BILL</h1>
+                <h3 align="center" style="letter-spacing:11px;font-weight:100">TABLE_NO: </h3>${ans[0].Table_no}
+
+                <br>
+                <h3 align="center" style="letter-spacing:11px;font-weight:100">ORDERS: </h3>
+                ${str}  
+                <h3 align="center" style="letter-spacing:11px;font-weight:100">AMOUNT :</h3>
+                ${ans1}
+                </div>
+                </body>
+            </html>
+        `)
+
+    })
 
 })
-
 
 
      app.listen(5456,function()
